4f9bb9c63476571fae299b9b09954578370aa123
qulitao88 <195731479@qq.com>
2019-05-06 19:47:07 +0800

    1

--- opms-bms/src/main/java/com/opms/bms/OpmsBmsApplication.java
+++ opms-bms/src/main/java/com/opms/bms/OpmsBmsApplication.java
@@ -3,11 +3,15 @@ package com.opms.bms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableAsync
 public class OpmsBmsApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(OpmsBmsApplication.class, args);
 	}
 
--- opms-ict/src/main/java/com/opms/ict/OpmsIctApplication.java
+++ opms-ict/src/main/java/com/opms/ict/OpmsIctApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpmsIctApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(OpmsIctApplication.class, args);
 	}
 
